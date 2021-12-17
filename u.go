52b1ded05ebf2e2da7597fb1a1aa0e126b6a5d3f
王哈哈 <656562721@qq.com>
2021-12-17 17:00:10 +0800

    update

--- u.go
+++ u.go
@@ -232,6 +232,6 @@ func DebugCyan(data ...interface{}) {
 
 // Version 输出版本
 func Version() {
-	fmt.Println("v0.0.6")
-	fmt.Println("Last update time: 2021-12-17 16:50:00")
+	fmt.Println("v0.0.7")
+	fmt.Println("Last update time: 2021-12-17 16:59:00")
 }
