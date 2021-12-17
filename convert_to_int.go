de161186e13c9238b1dde8daa29268734600b535
王哈哈 <656562721@qq.com>
2021-12-17 09:54:20 +0800

    update

--- convert_to_int.go
+++ convert_to_int.go
@@ -82,3 +82,78 @@ func ToInt64(val interface{}) int64 {
 		return 0
 	}
 }
+
+// ToUint to uint
+func ToUint(val interface{}) uint {
+	return uint(ToUint64(val))
+}
+
+// ToUint8 to uint8
+func ToUint8(val interface{}) uint8 {
+	return uint8(ToUint64(val))
+}
+
+// ToUint16 to uint16
+func ToUint16(val interface{}) uint16 {
+	return uint16(ToUint64(val))
+}
+
+// ToUint32 to uint32
+func ToUint32(val interface{}) uint32 {
+	return uint32(ToUint64(val))
+}
+
+// ToUint64 to uint64
+func ToUint64(val interface{}) uint64 {
+	switch v := val.(type) {
+	case string:
+		str := val.(string)
+		matched, _ := regexp.MatchString(`^[0-9.]+$`, str)
+		if !matched {
+			return 0
+		}
+
+		if strings.Count(val.(string), ".") <= 1 {
+			floatVal, err := strconv.ParseFloat(str, 64)
+			if err != nil {
+				return 0
+			}
+			return uint64(floatVal)
+		}
+		return 0
+	case int:
+		return uint64(val.(int))
+	case int8:
+		return uint64(val.(int8))
+	case int16:
+		return uint64(val.(int16))
+	case int32:
+		return uint64(val.(int32))
+	case int64:
+		return uint64(val.(int64))
+	case uint:
+		return uint64(val.(uint))
+	case uint8:
+		return uint64(val.(uint8))
+	case uint16:
+		return uint64(val.(uint16))
+	case uint32:
+		return uint64(val.(uint32))
+	case uint64:
+		return v
+	case float32:
+		return uint64(val.(float32))
+	case float64:
+		return uint64(val.(float64))
+	case []byte:
+		buffer := bytes.NewBuffer(val.([]byte))
+		var data uint64
+		err := binary.Read(buffer, binary.BigEndian, &data)
+		if err != nil {
+			return 0
+		}
+		return uint64(data)
+	default:
+		return 0
+	}
+}
