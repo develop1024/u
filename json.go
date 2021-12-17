1d599b32604ed7ccf7b05dbbff7787f6b1b7dbd2
王哈哈 <656562721@qq.com>
2021-12-17 15:06:26 +0800

    update

--- json.go
+++ json.go
@@ -10,6 +10,21 @@ func JsonGet(str, key string) gjson.Result {
 	return gjson.Get(str, key)
 }
 
+// JsonBytesGet 获取指定key的值
+func JsonBytesGet(data []byte, key string) gjson.Result {
+	return gjson.GetBytes(data, key)
+}
+
+// JsonGetMany 获取多个key值
+func JsonGetMany(str string, key ...string) []gjson.Result {
+	return gjson.GetMany(str, key...)
+}
+
+// JsonGetManyBytes 获取多个key值
+func JsonGetManyBytes(data []byte, key ...string) []gjson.Result {
+	return gjson.GetManyBytes(data, key...)
+}
+
 // JsonExists 是否存在
 func JsonExists(str, key string) bool {
 	return gjson.Get(str, key).Exists()
